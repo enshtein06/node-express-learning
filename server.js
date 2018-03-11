e5e332b84d66c5f0bfcc113db4874fd848dc96d3
Assiljbekov Askhat <enshtein06@gmail.com>
2018-03-11 20:16:52 +0700

    Run the server + hbs format

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
@@ -8,6 +9,26 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');//say what view engine i'm gonna use
 //hbs stands of handlebarsjs
+
+//logger
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server');
+		}
+	});
+
+	next();
+});//register expressMiddleWare
+/* .use fired when page are loaded */
+/* Takes 1 func!!! next uses to say that app should continue to run*/
+
+/*app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});*/ // stop the site
 
 app.use(express.static(__dirname + '/public')); //give access to public folder
 /* I can get those file by typing localhost:3000/index.html*/
